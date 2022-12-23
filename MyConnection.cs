466bbaad7e4c9b6e5b8bbfb1b8f82c10436a107d
Youssef <youssefalsoufi.1@gmail.com>
2022-12-23 14:54:34 +0100

    Upload all realted files.

---
+++ MyConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class MyConnection
+    {
+        public SqlConnection GetConnection()
+        {
+           return new SqlConnection() { ConnectionString = "server=DESKTOP-DTPF1JL; database=Converter; integrated Security= true" };
+        }
+    }
+}
