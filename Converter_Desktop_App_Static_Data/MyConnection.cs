8d9b030e5a03568f3a8a945a516d5fb19e42ab0f
Youssef <youssefalsoufi.1@gmail.com>
2023-03-22 22:40:21 +0100

    Add a Connction file

---
+++ Converter_Desktop_App_Static_Data/MyConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class MyConnection
+{
+	public MyConnection()
+	{
+		//
+		
+        /*
+        private void LoadConnection (object sender, EventArgs e)
+        {
+            
+            SqlConnection connect = new MyConnection().GetConnection();
+            connect.Open();
+            SqlCommand unitType = new SqlCommand("SELECT * FROM Units_Type", connect);
+            Console.WriteLine(unitType);
+            SqlDataReader readData = unitType.ExecuteReader();
+            while (readData.Read())
+            {
+                From.Items.Add(readData["Units_Type"]);
+            }
+            connect.Close();
+        }
+        */
+
+		//
+	}
+}
